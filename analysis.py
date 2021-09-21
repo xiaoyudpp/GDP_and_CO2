3f309afa6ff4add7e12baa92525816a64b2c5488
da pang chen <dapangchen@dapangs-MacBook-Pro.local>
2021-09-21 13:26:08 -0400

    delete suqare brackets

--- analysis.py
+++ analysis.py
@@ -1,16 +1,16 @@
 
 from altair.vegalite.v4.schema.core import Data
 import pandas as pd
-​
+
 # import data
 data = pd.read_csv(
     "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
 )
-​
+
 import altair as alt
 import numpy as np
-​
-GDPcap = data["GDP per capita (constant 2010 US$)" ""]
+
+GDPcap = data["GDP per capita (constant 2010 US$)"]
 data["Log GDP Per Capita"] = np.log(GDPcap)
 my_chart = (
     alt.Chart(data)
